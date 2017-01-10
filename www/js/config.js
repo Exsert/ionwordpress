5776bfbde623d722a2dc0f12d35b5b411de03db6
Exsert <danjenkins1993@gmail.com>
2017-01-10 20:00:33 +0000

    Initial Commit
    DarkCard Application Initial Commit

---
+++ www/js/config.js
@@ -0,0 +1,6 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'https://darkcard.net/api/')
+.constant('WORDPRESS_PUSH_URL', 'https://darkcard.net/push/')
+.constant('GCM_SENDER_ID', '574597432927')
+
+;
